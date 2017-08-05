4c69fcf35a82def53e97684c521655354c07915e
Anand <anand.pal2050@gmail.com>
2017-08-05 17:42:59 +0530

    initial commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
-$(document).ready(function() {
-
-});
+function copyToClipboard(element) {
+  var $temp = $("<input>");
+  $("body").append($temp);
+  $temp.val($(element).text()).select();
+  document.execCommand("copy");
+  $temp.remove();
+  alert('copied');
+}
