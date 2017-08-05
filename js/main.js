1f0c9fe74bf3cdeb8fc970ee1822932e06a95a5d
Anand <anand.pal2050@gmail.com>
2017-08-05 21:04:29 +0530

    initial commit

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
+//Copy the color
+
 function copyToClipboard(element) {
   var $temp = $("<input>");
   $("body").append($temp);
   $temp.val($(element).text()).select();
   document.execCommand("copy");
   $temp.remove();
-  alert('copied');
+  var getId = $(element).attr('id');
+  $('.text').html('copied');
+  setTimeout("location.reload(true);",1000);
 }
+
+
+//Search Engine Modification
+
+$('input').on('click',function() {
+  $(this).css('border-color','#fff');
+  $('.btn').css('border','none');
+});
